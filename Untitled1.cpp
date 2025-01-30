10d7873ffe446b9281cd52e91108798bff0af62f
MakuroKun <apisakwoot145@gmail.com>
2025-01-30 16:03:29 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+   cout<<"apisakwoot watcharachaiwaphon no 14"<<endl;
+   int number;
+   cout<<"--Program for Manu--"<<endl;
+   cout<<"Pless Number1.Program Area of Triangle"<<endl;
+   cout<<"Pless Number2.Program PlusNumber";
+   cin>> number;
+   if(number==1)
+   { float hight,baes,area;
+   cout<<"--program Triangle--"<<endl;
+   cout<<"Pless Iput high =";cin>>hight;
+   cout<<"Pless Iput baes =";cin>>baes;
+   area=0.5*hight*baes                                                       ;
+   cout<<"Ans.Triangle"<<area;
+   }
+   if(number==2)
+   {
+   	
+   	
+   }
+   if(number!=1 and number!=2)
+   cout<<"Eror"<<endl;
+
+}
