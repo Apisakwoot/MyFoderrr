6517a358afee245fd222a8d84ac5b511d751de55
Apisakwoot <apisakwoot145@gmail.com>
2025-12-24 20:58:12 +0700

    Update Forward.ino

--- Forward.ino
+++ Forward.ino
@@ -4,6 +4,6 @@ void StartForward() {
   digitalWrite(Moter_L_IN2, LOW);
   digitalWrite(Moter_R_IN1, HIGH); // มอเตอร์ขาวเดิน
   digitalWrite(Moter_R_IN2, LOW);
-  analogWrite(Moter_L_ENA, 100);
-  analogWrite(Moter_R_ENB, 100);
+  analogWrite(Moter_L_ENA, 104);
+  analogWrite(Moter_R_ENB, 107);
 }
